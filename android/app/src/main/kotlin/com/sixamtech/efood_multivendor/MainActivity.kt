3d510e1eb1ec396700a2ca1badfbaa622dceef12
romeuwb <romeuwb@msn.com>
2024-02-20 13:46:36 -0300

    alteração nome da bliblioteca

--- android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
+++ android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.sixamtech.stack_food
+package com.pedelogo.pede_logo
 
 import io.flutter.embedding.android.FlutterActivity
 
--- build/app/generated/source/buildConfig/release/com/sixamtech/stack_food/BuildConfig.java
+++ build/app/generated/source/buildConfig/release/com/sixamtech/stack_food/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.sixamtech.stack_food;
+package com.pedelogo.pede_logo;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.sixamtech.stack_food";
+  public static final String APPLICATION_ID = "com.pedelogo.pede_logo";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 4001;
   public static final String VERSION_NAME = "1.0.0";
