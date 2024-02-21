673566bdb4d83ad2e4ff878c53fcc0b769af152d
romeuwb <romeuwb@msn.com>
2024-02-21 08:59:14 -0300

    clean

--- Restaurant app/android/app/src/main/kotlin/com/u6amtech/efood_multivendor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pedelogo.pede_logo_restaurante
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pedelogo.pede_logo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
